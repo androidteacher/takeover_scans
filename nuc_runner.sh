b61d7d6dc88ea4ca1e465ee39f1066d96ced4afc
CyberLessons101 <joshbeck2006+androidclass@gmail.com>
2023-02-21 09:09:32 -0600

    Update nuc_runner.sh

--- nuc_runner.sh
+++ nuc_runner.sh
@@ -6,7 +6,7 @@ cp current_check/current_list.txt current_check/previous_lists/current_list-${no
 for x in $(cat current_check/current_list.txt); 
 do
 nuclei -u $x -t detect-all-takeovers.yaml | tee -a results/results.txt
-sed -i "s/$x//g" current_check/current_list.txt
-sed -i '/^$/d' current_check/current_list.txt
+tail -n +2 current_check/current_list.txt > current_check/currentlist_prev.txt
+cp current_check/currentlist_prev.txt current_check/current_list.txt
 done
 
